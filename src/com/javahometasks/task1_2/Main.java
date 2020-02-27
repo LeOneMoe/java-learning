274626bda8e47c01c2397368cb532177cd561554
LeOneMoe <baNANAmanchic@gmail.com>
2020-02-28 02:38:47 +0300

    task 2

--- src/com/javahometasks/task1_2/Main.java
+++ src/com/javahometasks/task1_2/Main.java
@@ -1,7 +1,40 @@
 package com.javahometasks.task1_2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
 
+        String pattern = "[aeiouy]";
+
+        String word = "palindrome";
+        System.out.println(word);
+
+        char symbols[] = word.toCharArray();
+
+        List<String> allMatches = new ArrayList<>();
+
+        Matcher m = Pattern.compile(pattern).matcher(word);
+
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+
+        Collections.reverse(allMatches);
+
+        int matchesIndex = 0;
+
+        for (int i = 0; i < symbols.length; i++) {
+            if (String.valueOf(symbols[i]).matches(pattern)) {
+                symbols[i] = allMatches.get(matchesIndex).charAt(0);
+                matchesIndex++;
+            }
+        }
+
+        System.out.println(symbols);
     }
 }
